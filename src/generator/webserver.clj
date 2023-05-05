3179e8f9a417e5839329bfe29032a7f2d6def135
tommi.martin <tommi-martin@users.noreply.github.com>
2023-05-05 14:12:07 +0300

    clean up webserver definition

--- src/generator/webserver.clj
+++ src/generator/webserver.clj
@@ -1,8 +1,6 @@
-;; Local webserver definition. This class works by defining a local webserver. The server gets it's handler from stasis with assets wrapped by optimus and jetty's middleware to get the right headers. Once the handler is ready. it is wrapped with reload and stacktrace functions from ring middleware. After all definitions starting the application can be done by runnint .start and .stop from the end of this file. 
+;; Local webserver definition. This class works by defining a local webserver. The server gets it's handler from stasis with assets wrapped by optimus and jetty's middleware to get the right headers. Once the handler is ready. it is wrapped with reload and stacktrace functions from ring middleware. After all definitions starting the application can be done by running .start and .stop from the end of this file. 
 (ns generator.webserver
   (:require
-   [ring.middleware.content-type :refer [wrap-content-type]]
-   [ring.middleware.not-modified :refer [wrap-not-modified]]
    [stasis.core :as stasis]
    [optimus.prime :as optimus]
    [optimus.assets :as assets]
@@ -11,8 +9,10 @@
    [optimus.export]
    [generator.pages :refer [get-pages]]
    [ring.adapter.jetty :as jetty]
-   [ring.middleware.stacktrace :as stacktrace]
-   [ring.middleware.reload :as reload]))
+   [ring.middleware.stacktrace :refer [wrap-stacktrace]]
+   [ring.middleware.reload :refer [wrap-reload]]
+   [ring.middleware.content-type :refer [wrap-content-type]]
+   [ring.middleware.not-modified :refer [wrap-not-modified]]))
 
 (defn get-assets []
   (assets/load-assets "public" ["/assets/main.css" 
@@ -21,14 +21,13 @@
 (def app (-> (stasis/serve-pages get-pages) ;; should return map of slug -> render call
              (optimus/wrap get-assets optimizations/all serve-live-assets)
              (wrap-content-type)
-             (wrap-not-modified)))
+             (wrap-not-modified)
+             (wrap-reload {:dirs ["src/generator"]})
+             (wrap-stacktrace)))
 
 ;; defines the local server to start by default the server is accessible from http://localhost:8000 after running .start on the server from comments below
-(defonce server 
-  (let [handler (stacktrace/wrap-stacktrace(reload/wrap-reload 
-                                            #'app
-                                            {:dirs ["src/generator"]}))]
-    (jetty/run-jetty handler {:port 8000 :join? false})))
+(defonce server
+  (jetty/run-jetty #'app {:port 8000 :join? false}))
   
 (comment (.start server))
 (comment (.stop server))
