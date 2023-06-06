4aafbb257640d0dea15292277b9a00a477c13fa3
tommi.martin <tommi-martin@users.noreply.github.com>
2023-06-06 16:04:54 +0300

    Reduce repeat in javascript

--- resources/public/assets/js/async-form.js
+++ resources/public/assets/js/async-form.js
@@ -1,5 +1,6 @@
 const submitForm = (form) => {
     const formData = new FormData(form);
+    const errorNode = document.createTextNode("Something went wrong while submitting your form, please try again.");
     form.classList.add("loading");
 
     fetch(form.action, {
@@ -19,16 +20,14 @@ const submitForm = (form) => {
           console.error('Form submission error:', response.status);
           form.classList.remove("loading");
           form.parentElement.classList.add("error");
-          var errorText = document.createTextNode("Something went wrong while submitting your form, please try again.");
-          form.children[0].children[0].appendChild(errorText);
+          form.children[1].children[0].appendChild(errorNode);
         }
     })
     .catch(function(error) {
         console.error('Form submission error:', error);
         form.classList.remove("loading");
         form.parentElement.classList.add("error");
-        var errorText = document.createTextNode("Something went wrong while submitting your form, please try again.");
-        form.children[0].children[0].appendChild(errorText);
+        form.children[1].children[0].appendChild(errorNode);
         // Display error message or perform other error handling
     });
 
