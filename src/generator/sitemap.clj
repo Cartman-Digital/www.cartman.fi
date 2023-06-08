8286350a50726b403ddb88968b88b0e4e02878f8
Paavo Pokkinen <paavo.pokkinen@hallatek.com>
2023-06-08 09:48:10 +0300

    minor refactor

--- src/generator/sitemap.clj
+++ src/generator/sitemap.clj
@@ -14,11 +14,12 @@
    :changefreq (:seoUpdateFrequency sitemap-page)
    :priority (:sitemapPriority sitemap-page)})
 
-(defn -main 
+(defn -main
   [target-dir]
-  (let [sitemap-input (map format-sitemap (remove #(false? (:seoIndexing %)) (get-in (get-contentful :sitemap-query) [:pageCollection :items])))
+  (let [data (get-in (get-contentful :sitemap-query) [:pageCollection :items])
+        sitemap-input (map format-sitemap (remove #(false? (:seoIndexing %)) data))
         sitemap (sitemap.core/generate-sitemap sitemap-input)
-        validation-result (sitemap.validator/validate-sitemap sitemap)] 
-    (if (> (count validation-result) 0) 
+        validation-result (sitemap.validator/validate-sitemap sitemap)]
+    (if (> (count validation-result) 0)
       (throw (IllegalArgumentException. (str (count validation-result) " Errors detected in site-map")))
-      (sitemap.core/save-sitemap (File. (str target-dir "sitemap.xml")) sitemap ))))
+      (sitemap.core/save-sitemap (File. (str target-dir "sitemap.xml")) sitemap))))
