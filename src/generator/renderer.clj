bd491b72022e9d89612ebd48beb4249eb4b25d34
catalina riveros <cat-rivers@users.noreply.github.com>
2023-08-18 13:59:43 +0300

    Divide people list and person view rendering

--- src/generator/renderer.clj
+++ src/generator/renderer.clj
@@ -276,20 +276,32 @@
     [:section.people-list-wrap 
      (into [:ul.people-list] (mapv render (get-in contentful-map [:peopleList :linkedFrom :personCollection :items])))]))
 
-;; Renders People-list and person page
+;;person-singleview and people-list view repeat to make styling and content changes easier
+(defn person-single-view
+  [args]
+  [:div.person
+   [:a {:href (create-url (:slug args))} [:h2 (:name args)]]
+   [:div.image
+    [:img {:alt (get-in args [:picture :title]) :src (:url (:picture args))}]]
+   [:div.body
+    [:h4 (:jobTitle args)]
+    [:p.office (:name (:office args))]
+    [:div.person-body (richtext->html (:json (:description args)))]]])
+
+(defn people-list-view
+  [args]
+  [:li.person-body
+   [:a {:href (create-url (:slug args))} [:h3 (:name args)]]
+   [:div.image
+    [:img {:alt (get-in args [:picture :title]) :src (:url (:picture args))}]]
+   [:div.body
+    [:h4 (:jobTitle args)]
+    [:div.person-body (richtext->html (:json (:shortText args)))]]])
+
 (defmethod render "Person"
   [args]
-  (let [fullbody (empty? (:shortText args))
-        content (if (empty? (:shortText args)) (:description args) (:shortText args))]
-    [(if fullbody :div.person :li.person-body)
-     [:a {:href (create-url (:slug args))} (if fullbody [:h2 (:name args)] [:h3 (:name args)])]
-     [:div.image
-      [:img {:alt (get-in args [:picture :title]) :src (:url (:picture args))}]]
-     [:div.body
-      (into [:div.types] (mapv #(vector :span {:class (str "type " %)} %) (:type args)))
-      [:h4 (:jobTitle args)]
-      [:p.office (:name (:office args))]
-      [:div.person-body (richtext->html (:json content))]]]))
+  (let [fullbody (empty? (:shortText args))]
+    (if fullbody (person-single-view args) (people-list-view args))))
 
 (comment (render {:__typename "ArticleList" :sys {:id "4N25RfloTD2aq3YtrDEzLk"} :numberOfPostsShown 3}))
 (comment (contentful/get-contentful :posts-by-list-query {:listId "4N25RfloTD2aq3YtrDEzLk"
