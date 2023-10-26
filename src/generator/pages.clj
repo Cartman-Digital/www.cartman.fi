49f1d6c239bd6245352f8a8a7c07ed2456047d8a
catalina riveros <cat-rivers@users.noreply.github.com>
2023-10-26 09:33:37 +0300

    Implement createPersonPage toggle in contentful, and associated render/no render

--- src/generator/pages.clj
+++ src/generator/pages.clj
@@ -116,10 +116,11 @@
   "Returns map of filename (eg. index.html) -> html"
   [m]
   (let [people-data (contentful/get-contentful :person-collection-query {:list false})
-        people (have vector? (get-in people-data [:personCollection :items]))]
+        people (have vector? (get-in people-data [:personCollection :items]))
+        filtered-people (filter #(get-in % [:createPersonPage]) people)]
     (into m (mapv #(vector
                     (str "/" (:slug %) ".html")
-                    (fn [context] (render-page % "person-page"))) people))))
+                    (fn [context] (render-page % "person-page"))) filtered-people))))
 
 (defn get-pages
   "Creates a map of page slug -> render calls."
